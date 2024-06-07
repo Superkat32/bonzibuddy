6b2db8fc02dd5aca3a48daebac1909870a3d75d1
Andrew Grant <agrantnmac@gmail.com>
2024-06-06 22:34:21 -0400

    initial bonzi buddy GUI override code
    just testing with swiftness instead of planned custom StatusEffect

---
+++ src/main/java/net/superkat/bonzibuddy/mixin/InventoryScreenMixin.java
@@ -0,0 +1,50 @@
+package net.superkat.bonzibuddy.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
+import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(InventoryScreen.class)
+public abstract class InventoryScreenMixin extends AbstractInventoryScreen<PlayerScreenHandler> implements RecipeBookProvider {
+
+    public InventoryScreenMixin(PlayerScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
+        super(screenHandler, playerInventory, text);
+    }
+
+    @WrapOperation(
+            method = "drawBackground(Lnet/minecraft/client/gui/DrawContext;FII)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V")
+    )
+    protected void bonzibuddy$drawBackground(DrawContext context, Identifier texture, int x, int y, int u, int v, int width, int height, Operation<Void> original) {
+        if(this.client != null) {
+            if(this.client.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+                Identifier attempt_texture = Identifier.of("bonzibuddy", texture.getPath());
+                MinecraftClient.getInstance().getTextureManager().bindTexture(attempt_texture);
+                // horrible hack to check if bonzi override exists
+                if(!(MinecraftClient.getInstance().getTextureManager().getOrDefault(attempt_texture, null) instanceof NativeImageBackedTexture)) {
+                    texture = attempt_texture;
+                }
+                original.call(context, texture, x, y, u, v, width, height);
+                } else {
+                    original.call(context, texture, x, y, u, v, width, height);
+                }
+        } else {
+            original.call(context, texture, x, y, u, v, width, height);
+        }
+    }
+
+}
